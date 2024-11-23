174297f07776c1c65ca85dcbe0e9a0f63b3dbb08
VictorFil67 <victorfilinovych@gmail.com>
2024-11-23 19:28:26 +0200

    added get query

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
-// import { listen } from "engine.io";
 import express from "express";
 
 const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Hello server!");
+  //   res.send("Hello server!");
+  res.json("Hello server!");
+});
 
 app.listen(5000, () => console.log("Server is  running at port 5000"));
