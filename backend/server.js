7a3857e9f3e2451d836240fcf05fbb1f907d7fdb
VictorFil67 <victorfilinovych@gmail.com>
2024-11-23 19:41:46 +0200

    created authRouter

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import authRouter from "./routes/authRouter";
 
 const app = express();
+
+app.use("api/users", authRouter);
 
 app.get("/", (req, res) => {
   console.log("Hello server!");
