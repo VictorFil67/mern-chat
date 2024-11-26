8862828ed27ebd19a51833f872ade48290447295
VictorFil67 <victorfilinovych@gmail.com>
2024-11-26 14:26:32 +0200

    added protectRoute middleware

--- backend/controllers/messageControllers.js
+++ backend/controllers/messageControllers.js
@@ -1,5 +1,7 @@
 const sendMessage = async (req, res) => {
   const { id } = req.params;
+  const { message } = req.body;
+
   console.log(`The message sent to user ${id}`);
   await res.json(`The message sent to user ${id}`);
 };
---
+++ backend/middlewares/protectRoute.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const { JWT_SECRET } = process.env;
+
+const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized: No token provided" });
+    }
+
+    const detected = jwt.verify(token, JWT_SECRET);
+    console.log(detected);
+    if (detected) {
+      return res.status(401).json({ error: "Unauthorized: invalid token" });
+    }
+    const { userId } = detected;
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    req.user = user;
+  } catch (error) {
+    console.log("Error in protectRoute middleware: ", error.message);
+    res.status(500).json("Internal server error");
+  }
+};
+
+export default protectRoute;
